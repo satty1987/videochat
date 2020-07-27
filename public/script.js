a4c3a6fdbc738ed3b3d03ec9d58e4d0461ebd0a9
Satnam Singh <satnam.singh156@t-mobile.com>
2020-07-27 15:34:18 -0700

    update

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: 'ng-git.herokuapp.com',
-    port: '443'
+    port: '443',
+    secure: true, 
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
